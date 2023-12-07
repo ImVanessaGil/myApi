2effdd41d45573a0aa30919d412b334748fe95d3
ImVanessaGil <152836576+ImVanessaGil@users.noreply.github.com>
2023-12-06 23:34:42 -0600

    Api Pokemon Lado Servidor (Backend)
    Api Realizada con Node.js, Express.js utilizando Mongo db Atlas

---
+++ datos.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+        name: 'Balbasaur',
+        type: 'Planta',
+        number: 1
+    },
+    {
+        name: 'Charmander',
+        type: 'Fuego',
+        number: 4
+    },
+    {
+        name: 'Squirtle',
+        type: 'Agua',
+        number: 7
+    },
+    {
+        name: 'Metapod',
+        type: 'Bicho',
+        number: 11
+    },
+    {
+        name: 'Sandshrew',
+        type: 'Tierra',
+        number: 27
+    },
+    {
+        name: 'Clefairy',
+        type: 'Hada',
+        number: 35
+    }
+];
+
+module.exports = { data };
